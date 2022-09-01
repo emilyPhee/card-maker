64d89160a4ebfcc0d2a3e4ead21ade0a7834ab3f
Emily Phee <ephee@uci.edu>
2022-08-31 17:07:34 -0700

    navigate to Card Editor component when user login with Google/Github button

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+  let navigate = useNavigate();
+
   const onLogin = event => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log());
+      .then(result => {
+        const credential = result.credential;
+        const token = credential.accessToken;
+        const user = result.user;
+
+        if (user) {
+          navigate('/editor', { replace: true });
+        }
+      })
+      .catch(error => {
+        // Handle error here
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+      });
   };
+
   return (
     <section className={styles.loginContainer}>
       <Header />
@@ -17,11 +38,12 @@ const Login = ({ authService }) => {
         <h1>Login</h1>
         <ul>
           <li>
-            <Link to="/editor">
-              <button className={styles.googleLogin} onClick={onLogin}>
-                Google
-              </button>
-            </Link>
+            {/* <Link to="/editor"> */}
+
+            <button className={styles.googleLogin} onClick={onLogin}>
+              Google
+            </button>
+            {/* </Link> */}
           </li>
           <li>
             <button className={styles.githubLogin} onClick={onLogin}>
